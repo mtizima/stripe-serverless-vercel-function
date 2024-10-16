d7a1b3d52e4b9d66bb5685ccbd1d4a78df5ef538
Grebenshchikov Dmitry <mtizima@gmail.com>
2024-10-16 20:03:57 +0300

    Mv CORS policy to js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,21 +1,31 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (req.method === 'POST') {
-        const { amount, currency } = req.body;
+        const { amount, currency, name, email } = req.body;
         try {
-            // Create a Payment Intent
             const paymentIntent = await stripe.paymentIntents.create({
                 amount,
                 currency,
+                automatic_payment_methods: { enabled: true },
+                metadata: { name, email },
             });
-            // Send the client_secret back to the frontend
             res.status(200).json({ clientSecret: paymentIntent.client_secret });
         } catch (error) {
             res.status(400).json({ error: error.message });
         }
     } else {
-        res.setHeader('Allow', 'POST');
+        res.setHeader('Allow', 'OPTIONS, POST');
         res.status(405).end('Method Not Allowed');
     }
 }
